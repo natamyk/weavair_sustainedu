6d8d8a3ed385be9f384f0b1ee11999f858fab912
natamyk <natalia.mykhaylova@utoronto.ca>
2024-10-25 00:27:18 -0400

    Create content_management.py

---
+++ docs/content_management.py
@@ -0,0 +1,4 @@
+class ContentManagement:
+    def create_content(self, topic):
+        # Logic to generate content using IBM Watsonx
+        return {"topic": topic, "content": f"Content related to {topic} generated!"}
